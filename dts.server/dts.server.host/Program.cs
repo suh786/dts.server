81b581f6c0611639beeb0effb55e9f785d3f529f
Suhail Siddiqi <suh786@gmail.com>
2013-06-11 17:39:35 +0530

    First Cut

---
+++ dts.server/dts.server.host/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace dts.server.host
+{
+    class Program
+    {
+        private static Uri baseAddress = new Uri("http://localhost:3030/RegistrationService");
+
+        static void Main(string[] args)
+        {
+            // Create the ServiceHost.
+            using (ServiceHost host = new ServiceHost(typeof(RegistrationService), baseAddress))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+}
